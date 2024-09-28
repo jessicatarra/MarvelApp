53b40998b5a8efda0689e83f1cf8206ef9227c94
Jessica Tarra <jessica@tarralva.com>
2024-09-28 10:22:25 -0300

    first commit

---
+++ MarvelApp/MarvelAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  MarvelAppApp.swift
+//  MarvelApp
+//
+//  Created by Jessica Tarra on 28/09/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MarvelAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MarvelAppTests/MarvelAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppTests.swift
+//  MarvelAppTests
+//
+//  Created by Jessica Tarra on 28/09/24.
+//
+
+import Testing
+@testable import MarvelApp
+
+struct MarvelAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
